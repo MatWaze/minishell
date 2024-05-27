0d027a5a4123263ce21da9897cf68707e66594ad
zanikin <zaharanikinmai@gmail.com>
2024-05-27 19:57:41 +0400

    expansion code done, but untested

--- expansion/expansion.c
+++ expansion/expansion.c
@@ -6,16 +6,20 @@
 /*   By: zanikin <zanikin@student.42yerevan.am>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 15:49:32 by zanikin           #+#    #+#             */
-/*   Updated: 2024/05/27 15:32:08 by zanikin          ###   ########.fr       */
+/*   Updated: 2024/05/27 19:55:47 by zanikin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "expansion.h"
 
-size_t	count_expanded_string(const char *str, t_export **ev,
-			int error);
-int		is_inside_quotes(char type, char *qtype);
+size_t		count_expanded_string(const char *str, t_export **ev,
+				int error);
+int			is_inside_quotes(char type, char *qtype);
+static void	expand_envvar(const char **pstr, char **exp_str,
+				t_export **evlist, int error);
+static void	insert_envvar_val(const char **pstr, char **exp_str,
+				t_export **evlist);
 
 char	*expand(const char *str, t_export **evlist, int error)
 {
@@ -29,11 +33,8 @@ char	*expand(const char *str, t_export **evlist, int error)
 	qtype = 0;
 	while (*str)
 	{
-		if (*str == '\'' || *str == '"')
-		{
-			if (is_inside_quotes(*str, &qtype))
-				*exp_str++ = *str;
-		}
+		if ((*str == '\'' || *str == '"') && is_inside_quotes(*str, &qtype))
+			*exp_str++ = *str;
 		else if (!qtype && *str == '~' && (str[1] == ' ' || str[1] == '/'
 				|| !str[1]))
 		{
@@ -41,12 +42,52 @@ char	*expand(const char *str, t_export **evlist, int error)
 			exp_str += ft_strlcpy(exp_str, tmp, ft_strlen(tmp) + 1);
 		}
 		else if (*str == '$' && (!qtype || qtype == '"'))
-		{
-			tmp = extract_ev(&str);
-			
-		}
+			expand_envvar(&str, &exp_str, evlist, error);
+		else
+			*exp_str++ = *str;
 		str++;
 	}
+	return (exp_str);
+}
+
+static void	expand_envvar(const char **pstr, char **exp_str,
+					t_export **evlist, int error)
+{
+	char	*tmp;
+	char	type;
+
+	if (pstr[0][1] == '\'' || pstr[0][1] == '"')
+	{
+		type = (++pstr[0])[0];
+		while ((++pstr[0])[0] != type)
+			exp_str[0]++[0] = pstr[0][0];
+	}
+	else if (pstr[0][1] == '?')
+	{
+		pstr[0]++;
+		if (error <= 0)
+			exp_str[0]++[0] = '0' * (!error) + '-' * (error < 0);
+		while (error)
+		{
+			exp_str[0]++[0] = (error % 10) * (1 - 2 * (error < 0)) + '0';
+			error /= 10;
+		}
+	}
+	else if (!pstr[0][0] || pstr[0][0] == ' ' || pstr[0][0] == '$')
+		exp_str[0]++[0] = '$';
+	else
+		insert_envvar_val(pstr, exp_str, evlist);
+}
+
+static void	insert_envvar_val(const char **pstr, char **exp_str,
+				t_export **evlist)
+{
+	char	*name;
+
+	name = extract_ev(pstr);
+	exp_str[0] += ft_strlcpy(exp_str[0], name,
+			ft_strlen(get_value_from_key(evlist, name)) + 1);
+	free(name);
 }
 
 char	*extract_ev(const char **pstr)

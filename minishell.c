53689bb949d13a7ee111fe048b5c61e161f77097
MatWaze <mamazari@c2r12s4.42yerevan.am>
2024-06-22 15:19:23 +0400

    Added signal handling; also set args->p_count to 0 insize init_minishell before the call to set_pwds.

--- minishell.c
+++ minishell.c
@@ -6,12 +6,9 @@
 /*   By: mamazari <mamazari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 10:55:29 by mamazari          #+#    #+#             */
-/*   Updated: 2024/06/08 17:35:07 by mamazari         ###   ########.fr       */
+/*   Updated: 2024/06/22 15:02:11 by mamazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// prompt; history; run executables; redirections; ' and ";
-// pipes; env vars; $? ; ctrl-C; ctrl-D; ctrl-\; built-ins
 
 #include <sys/wait.h>
 #include <stdlib.h>
@@ -19,6 +16,8 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <signal.h>
+#include <unistd.h>
+#include <termios.h>
 
 #include "quotes/quotes.h"
 #include "pwd/pwd.h"
@@ -26,11 +25,13 @@
 #include "pipex/pipex.h"
 #include "common/common.h"
 
+int	g_exit_status = 0;
+
 static void	run_pipex(t_args *args, char **words, char *str);
 static void	init_minishell(char **envp, t_args *args);
 static int	main_loop(t_args *args);
 
-int	main(int argc, char **argv, char **envp)
+int	main2(int argc, char **argv, char **envp)
 {
 	t_args				args;
 	int					running;
@@ -40,15 +41,21 @@ int	main(int argc, char **argv, char **envp)
 	init_minishell(envp, &args);
 	running = 1;
 	while (running)
-	{
 		running = main_loop(&args);
-	}
 	ft_lstclear((t_list **)&args.export_list, free_export_content);
 	ft_lstclear((t_list **)&args.env_list, free_export_content);
 	ft_lstclear(&args.pids, free);
 	free(args.pids);
-	return (args.exit_code);
+	rl_clear_history();
+	return (g_exit_status);
 }
+
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	main2(argc, argv, envp);
+// 	system("leaks minishell");
+// 	return (0);
+// }
 
 static void	init_minishell(char **envp, t_args *args)
 {
@@ -58,6 +65,7 @@ static void	init_minishell(char **envp, t_args *args)
 	int			i;
 
 	i = 0;
+	args->p_count = 0;
 	args->quotes_closed = 0;
 	args->envp = envp;
 	export_list = NULL;
@@ -73,17 +81,60 @@ static void	init_minishell(char **envp, t_args *args)
 	args->env_list = env_list;
 	args->export_list = export_list;
 	args->pids = NULL;
-	args->exit_code = 0;
 	sort_list(&export_list);
 	set_pwds(args);
 }
 
+void	disable_echoctl(void)
+{
+	struct termios	t;
+
+	if (tcgetattr(STDIN_FILENO, &t) != 0)
+		return ;
+	t.c_lflag &= ~(ECHOCTL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &t);
+}
+
+static void	sigact_handler(int signum, siginfo_t *info, void *context)
+{
+	(void)info;
+	(void)context;
+	if (signum == SIGINT)
+	{
+		g_exit_status = 1;
+		write(1, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	else if (signum == SIGQUIT)
+	{
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+		rl_cleanup_after_signal();
+	}
+	disable_echoctl();
+}
+
+void	init_signals(void)
+{
+	struct sigaction	sa;
+
+	ft_memset(&sa, 0, sizeof(sa));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = sigact_handler;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+}
+
 static int	main_loop(t_args *args)
 {
-	char	*str;
-	size_t	size;
-	char	qstr[2];
+	char				*str;
+	size_t				size;
+	char				qstr[2];
 
+	init_signals();
 	str = readline("minishell$ ");
 	if (str)
 		size = ft_strlen(str);
@@ -95,10 +146,10 @@ static int	main_loop(t_args *args)
 		args->quotes_closed = quotes_type(str, str + size);
 		if (args->quotes_closed)
 		{
-			qstr[0] = (char)args->exit_code;
+			qstr[0] = (char)g_exit_status;
 			qstr[1] = '\0';
-			print_error_msg("Unclosed quote\n", (char *)qstr);
-			args->exit_code = 1;
+			print_error_msg("Unclosed quote", (char *)qstr);
+			g_exit_status = 1;
 		}
 		else
 			run_pipex(args, quoted_split(str, '|'), str);
@@ -111,8 +162,8 @@ static void	run_pipex(t_args *args, char **words, char *str)
 {
 	int		i;
 
-	args->p_count = 0;
 	i = 0;
+	args->p_count = 0;
 	args->argv = words;
 	while (str[i])
 	{
@@ -122,13 +173,12 @@ static void	run_pipex(t_args *args, char **words, char *str)
 	}
 	if (pipex(args) == 1)
 	{
-		print_error_msg("failed: Resource temporarily unavailable\n", \
-			"fork");
+		print_error_msg("failed: Resource temporarily unavailable", "fork");
 		kill_processes(args->pids);
-		args->exit_code = 1;
+		g_exit_status = 1;
 	}
 	else
-		get_exit_status(args);
+		get_exit_status(args->pids);
 	free_arr(args->argv);
 	ft_lstclear(&args->pids, free);
 }
